480ff142d8e8f9a9898a4d32684f32b2c6f1ea6e
Paulo Macedo <paulomacedo@ua.pt>
2023-05-02 00:44:39 +0100

    localização do dfis about.hmtl

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([40.63327715251739, -8.659529757486075], 18);
+var map = L.map('map').setView([40.63189212093659, -8.65822760177182], 17);
 
 var myIcon = L.icon({
     iconUrl: '../assets/icons/university.png',
@@ -12,6 +12,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
 
 L.marker([40.63327715251739, -8.659529757486075],
+    { icon: myIcon }
+).addTo(map)
+L.marker([40.630261824695495, -8.65669602425622],
     { icon: myIcon }
 ).addTo(map)
 
